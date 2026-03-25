d1badeabeaf083b3288099e9ad62002ab791fdad
Dino Zvonar <62692198+DinoDominicZvonar@users.noreply.github.com>
2026-03-25 23:48:22 +0100

    Removed unused code

--- code.js
+++ code.js
@@ -1,21 +1,3 @@
-function HomeClick(el) 
-{
-    if (!el.classList.contains("disabled")) return;
-    window.location.href = "https://dinozvonar.com/";
-}
-
-function AboutClick(el) 
-{
-    if (!el.classList.contains("disabled")) return;
-    window.location.href = "https://dinozvonar.com/about";
-}
-
-function GamesClick(el) 
-{
-    if (!el.classList.contains("disabled")) return;
-    window.location.href = "https://dinozvonar.com/games";
-}
-
 function HamburgerClick(el) 
 {
     const nav = document.getElementsByClassName("mobileNavMenu")[0];
