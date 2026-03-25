23999da5afd93b960d343674354c448bd146dd4c
Dino Zvonar <62692198+DinoDominicZvonar@users.noreply.github.com>
2026-03-25 23:41:16 +0100

    Fixed mobile navbar?

--- code.js
+++ code.js
@@ -15,3 +15,21 @@ function GamesClick(el)
     if (!el.classList.contains("disabled")) return;
     window.location.href = "https://dinozvonar.com/games";
 }
+
+function HamburgerClick(el) 
+{
+    const nav = document.getElementsByClassName("mobileNavMenu")[0];
+
+    if (nav.classList.contains("closed"))
+    {
+        nav.classList.remove("closed");
+        el.src = "/Resources/Images/Hamburger Menu X.webp";
+        document.body.classList.add("noScroll");
+    }
+    else
+    {   
+        nav.classList.add("closed");
+        el.src = "/Resources/Images/Hamburger Menu.webp";
+        document.body.classList.remove("noScroll");
+    }
+}
